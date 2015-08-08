b8dfca33c5d16e29aa4eea0bbac10084c56e123e
Markeur123 <A_jt_18@hotmail.com>
2015-08-09 03:46:32 +0800

    add plot2.R

---
+++ plot2.R
@@ -0,0 +1,5 @@
+data <- read.table(text = grep("^[1,2]/2/2007", readLines("household_power_consumption.txt"), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
+data$datetime <- as.POSIXct(paste(as.Date(data$Date, format = "%d/%m/%Y"), data$Time))
+plot(data$datetime, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "plot2.png")
+dev.off()
